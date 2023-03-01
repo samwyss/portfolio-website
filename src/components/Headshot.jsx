43d7fe986e0cc4990eab9897929c5b370c3b1b9d
Sam Wyss <sjwyss@outlook.com>
2023-02-28 19:47:47 -0500

    added organization and a placeholder picture

---
+++ src/components/Headshot.jsx
@@ -0,0 +1,9 @@
+import HeadshotImg from '../assets/DefaultHeadshot.jfif'
+
+export default function Headshot() {
+  return (
+    <>
+      <img src={HeadshotImg} alt="Headshot Image" />
+    </>
+  );
+}
